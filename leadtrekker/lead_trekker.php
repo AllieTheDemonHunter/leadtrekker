779ef99c804ed959916a6d20b823a0c29c22ef41
allie.rockdev@gmail.com <allie.rockdev@gmail.com>
2016-06-10 09:00:16 +0200

    Init

---
+++ leadtrekker/lead_trekker.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allie
+ * Date: 16/06/03
+ * Time: 11:17 AM
+ */
+namespace leadtrekker;
+
+interface lead_trekker {
+  public function __construct($leadtrekker_api_key, $source_id, $lead_data, $debug = FALSE);
+
+  public function __destruct();
+
+  public function _connect();
+
+  public function query($function, $data = array());
+
+  /**
+   * Function which considers $_SESSION data for injection
+   * with *any* lead creation.
+   */
+  public function leadtrekker_inject_fields(&$lead_data);
+}
